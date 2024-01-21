ae9a51ed079bb6b8253b4373d9a026e2949bac35
covuhs <covuhs@gmail.com>
2024-01-21 18:36:10 +0100

    added a font, changed bg color, displaying a text

--- p5sketch.js
+++ p5sketch.js
@@ -1,7 +1,19 @@
+let helvetica
+let city = "Thing"
+
+function preload() {
+    font = loadFont('NeurealTrial-Mono.otf');
+}
+
 function setup() {
     createCanvas(500, 400);
   }
   
   function draw() {
-    background(mouseX);
+    background(50);
+    fill(200);
+    textFont(font);
+    textAlign(CENTER,CENTER);
+    textSize(60);
+    text(city,width/2,30);
   }
